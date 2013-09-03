b23a1e77b6c8bc6bec381888af1aeee0697dd56e
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-09-03 18:17:54 -0400

    Restructured

--- build/generate.sh
+++ build/generate.sh
@@ -1,2 +1,7 @@
 jison ../src/mochascriptParser.jison ../src/mochascriptLexer.jisonlex
-mv mochascriptParser.js ../dist
+
+cp ../src/lib/stdlib.js ../dist
+cp ../src/lib/stdlib-dashlo.js ../dist
+
+cat ../src/js/backend.js mochascriptParser.js > ../dist/mochascriptParser.js
+rm mochascriptParser.js
