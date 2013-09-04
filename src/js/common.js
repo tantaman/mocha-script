27dbc9b8f29b32a9cded091a4b87a17a263649f7
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-09-04 16:32:46 -0400

    Added shims for older borwsers

--- src/js/common.js
+++ src/js/common.js
@@ -28,3 +28,65 @@ function rest(arr, n) {
 
 	return result;
 }
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
+if (!Array.prototype.some) {
+  Array.prototype.some = function(fun /*, thisp */) {
+    'use strict';
+
+    if (this == null) {
+      throw new TypeError();
+    }
+
+    var thisp, i,
+        t = Object(this),
+        len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    thisp = arguments[1];
+    for (i = 0; i < len; i++) {
+      if (i in t && fun.call(thisp, t[i], i, t)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+}
+
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (fn, scope) {
+        'use strict';
+        var i, len;
+        for (i = 0, len = this.length; i < len; ++i) {
+            if (i in this) {
+                fn.call(scope, this[i], i, this);
+            }
+        }
+    };
+}
